258f8181cdf20eb8fcf07a5d4b01dcd906de2c9a
Maxim <38497831+StableBig@users.noreply.github.com>
2023-07-31 16:58:09 +0200

    make the code flatter

--- download_apod.py
+++ download_apod.py
@@ -4,23 +4,24 @@ import os
 import requests
 from space_image_utils import download_images_from_urls
 
-def fetch_nasa_images(api_key, date):
+
+def fetch_nasa_images(date, api_key):
     params = {
         "api_key": api_key,
         "date": date
     }
-
     response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
     response.raise_for_status()
     image_info_from_api = response.json()
 
-    if "url" in image_info_from_api and image_info_from_api["media_type"] == "image":
-        image_links = [image_info_from_api["url"]]
-        save_folder = "images"
-        filename_prefix = "apod"
-        download_images_from_urls(image_links, save_folder, filename_prefix)
-    else:
+    if not ("url" in image_info_from_api and image_info_from_api["media_type"] == "image"):
         print("Не найдено ссылок на изображения.")
+        return
+
+    image_links = [image_info_from_api["url"]]
+    save_folder = "images"
+    filename_prefix = "apod"
+    download_images_from_urls(image_links, save_folder, filename_prefix)
 
 if __name__ == "__main__":
     load_dotenv()
@@ -30,4 +31,4 @@ if __name__ == "__main__":
     parser.add_argument("--date", help="Дата изображения APOD (YYYY-MM-DD)", default="2023-07-30")
     args = parser.parse_args()
 
-    fetch_nasa_images(api_key, args.date)
+    fetch_nasa_images(args.date, api_key)
