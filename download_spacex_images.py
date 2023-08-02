241fc6d72f922b9d66868363759bc0f44e858031
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-02 14:58:28 +0200

    refactor the code by removing unnecessary function (SpaceX)
    Refactor the code by inlining image download and folder creation functions directly in main script (download_spacex_images.py)

--- download_spacex_images.py
+++ download_spacex_images.py
@@ -1,6 +1,9 @@
 import argparse
+from dotenv import load_dotenv
+import os
 import requests
-from space_image_utils import download_images_from_urls
+from datetime import datetime
+from space_image_utils import download_image
 
 
 def fetch_spacex_last_launch(launch_id):
@@ -9,18 +12,22 @@ def fetch_spacex_last_launch(launch_id):
     response.raise_for_status()
     spacex_response_content = response.json()
 
-    image_links = spacex_response_content.get("links", {}).get("flickr", {}).get("original", [])
-
-    if image_links:
-        save_folder = "images"
-        download_images_from_urls(image_links, save_folder, "spacex")
-    else:
-        print("Не найдено ссылок на изображения.")
+    return spacex_response_content.get("links", {}).get("flickr", {}).get("original", [])
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     parser = argparse.ArgumentParser(description="Скрипт для скачивания фотографий запусков с сайта SpaceX.")
     parser.add_argument("--launch_id", help="ID запуска SpaceX", default="5eb87d47ffd86e000604b38a")
     args = parser.parse_args()
 
-    fetch_spacex_last_launch(args.launch_id)
+    image_urls = fetch_spacex_last_launch(args.launch_id)
+
+    if image_urls:
+        os.makedirs("images", exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        for index, image_url in enumerate(image_urls, start=1):
+            download_image(image_url, "images", "spacex", timestamp, index)
+    else:
+        print("Не найдено ссылок на изображения.")
