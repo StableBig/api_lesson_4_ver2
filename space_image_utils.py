82e4f561bb7c8318031f61d9beb0e45380ab963e
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-02 14:39:36 +0200

    merge the functions into download_images_from_urls

--- space_image_utils.py
+++ space_image_utils.py
@@ -10,33 +10,21 @@ def get_file_extension_from_url(url):
     return file_extension
 
 
-def save_image(image_url, filename):
-    response = requests.get(image_url)
-    response.raise_for_status()
-
-    with open(filename, "wb") as file:
-        file.write(response.content)
-
-
 def download_image(image_url, save_folder, filename_prefix, timestamp, index):
     image_extension = get_file_extension_from_url(image_url)
     image_name = f"{filename_prefix}_{timestamp}_{index}{image_extension}"
     save_path = os.path.join(save_folder, image_name)
 
-    save_image(image_url, save_path)
+    response = requests.get(image_url)
+    response.raise_for_status()
 
-
-def create_directory_if_not_exists(directory):
-    os.makedirs(directory, exist_ok=True)
-
-
-def get_current_timestamp():
-    return datetime.now().strftime("%Y%m%d%H%M%S")
+    with open(save_path, "wb") as file:
+        file.write(response.content)
 
 
 def download_images_from_urls(image_urls, save_folder, filename_prefix):
-    create_directory_if_not_exists(save_folder)
-    timestamp = get_current_timestamp()
+    os.makedirs(save_folder, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
     for index, image_url in enumerate(image_urls, start=1):
         download_image(image_url, save_folder, filename_prefix, timestamp, index)
